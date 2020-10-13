655cffe644a39967be0a638da2da2928109023f9
Jacob Turner <44938917+Xychic@users.noreply.github.com>
2020-10-13 22:57:44 +0100

    Ensure logout even on ssh error

--- autoUserAssign.sh
+++ autoUserAssign.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
-userNum=-1
+if [[ -n $SSH_CONNECTION ]] ; then
+        userNum=-1
 
-for num in {0..100}
-do
-        if [[ ! "$(who | grep 'user*' | cut -d' ' -f1 | sort | uniq)" =~ "${num}" ]]; then
-                userNum=$num
-                break
+        for num in {0..100}
+        do
+                if [[ ! "$(who | grep 'user*' | cut -d' ' -f1 | sort | uniq)" =~ "${num}" ]]; then
+                        userNum=$num
+                        break
+                fi
+        done
+        if [[ "$userNum" == -1 ]]; then
+                echo "No free users available"
+                exit
+        else
+                echo "User 'user$userNum' is free"
+                echo "Logging in as 'user$userNum'..."
+                ssh user$userNum@127.0.0.1                                                                                                    
+                exit
         fi
-done
-if [[ "$userNum" == -1 ]]; then
-        echo "No free users available"
-        exit
-else
-        echo "User 'user$userNum' is free"
-        echo "Logging in as 'user$userNum'..."
-        ssh user$userNum@127.0.0.1 && exit
 fi
